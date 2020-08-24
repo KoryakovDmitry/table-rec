d8759d0edc3f7dc9f6ae824fdb9358d88f100646
Dmitry Koryakov <ararararagi@niuitmo.ru>
2020-08-24 12:24:05 +0300

    first stage

---
+++ border.py
@@ -0,0 +1,59 @@
+from borderfunc import extract_table, extractText, span
+import cv2
+
+
+# Input : table coordinates [x1,y1,x2,y2]
+# Output : XML intucture for ICDAR 19 single table
+def border(table, image):
+    image_np = image  # [table[1]-10:table[3]+10,table[0]-10:table[2]+10]
+    imag = image.copy()
+    final = extract_table(image_np, 1)
+    if final is None:
+        return None
+    X = []
+    Y = []
+    for x1, y1, x2, y2, x3, y3, x4, y4 in final:
+        if x1 not in X:
+            X.append(x1)
+        if x3 not in X:
+            X.append(x3)
+        if y1 not in Y:
+            Y.append(y1)
+        if y2 not in Y:
+            Y.append(y2)
+
+    X.sort()
+    Y.sort()
+    # print("X = ",X)
+    # print("Y = ",Y)
+
+    cv2.rectangle(imag, (table[0], table[1]), (table[2], table[3]), (0, 255, 0), 2)
+    structure = list()
+    for box in final:
+        if box[0] > table[0] - 5 and box[1] > table[1] - 5 and box[2] < table[2] + 5 and box[3] < table[3] + 5:
+            cellBox = extractText(imag[box[1]:box[3], box[0]:box[4]])
+            if cellBox is None:
+                continue
+            ## to visualize the detected text areas
+            cv2.rectangle(imag, (cellBox[0] + box[0], cellBox[1] + box[1]), (cellBox[2] + box[0], cellBox[3] + box[1]),
+                          (255, 0, 0), 2)
+
+            end_col, end_row, start_col, start_row = span(box, X, Y)
+
+            one = [int(cellBox[0] + box[0]), int(cellBox[1] + box[1])]
+            two = [int(cellBox[0] + box[0]), int(cellBox[3] + box[1])]
+            three = [int(cellBox[2] + box[0]), int(cellBox[3] + box[1])]
+            four = [int(cellBox[2] + box[0]), int(cellBox[1] + box[1])]
+
+            cell = {'end_col': [end_col, one],
+                    'end_row': [end_row, two],
+                    'start_col': [start_col, three],
+                    'start_row': [start_row, four]}
+
+            structure.append(cell)
+    ## to visualize the detected text areas
+    # cv2.imshow("detected cells",imag)
+    # cv2.waitKey(0)
+    return tuple(table), tuple(structure)
+
+# border([111,228,680,480],cv2.imread('cTDaR_t10039.jpg'))
---
+++ main.py
@@ -0,0 +1,64 @@
+import sys
+
+sys.path.append('/home/ochinchin/Initflow/OcrRPA/cascade20/table-rec/mmdetection')
+
+from border import border
+from mmdet.apis import inference_detector, init_detector
+import cv2
+from blessfunc import borderless
+import glob
+
+image_path = '/home/ochinchin/Initflow/OcrRPA/cascade20/table-rec/imgs/*'
+xmlPath = '/home/ochinchin/Initflow/OcrRPA/cascade20/table-rec/xml/'
+config_fname = '/home/ochinchin/Initflow/OcrRPA/cascade20/table-rec/cascade_mask_rcnn_hrnetv2p_w32_20e.py'
+checkpoint_path = "/home/ochinchin/Initflow/OcrRPA/cascade20/table-rec/epoch_36_mmdet20_converted.pth"
+device = 'cuda:0'
+threshold = .85
+
+
+def main(image_path=image_path, xmlPath=xmlPath, config_fname=config_fname, checkpoint_path=checkpoint_path,
+         device=device, threshold=threshold):
+    model = init_detector(config=config_fname, checkpoint=checkpoint_path, device=device)
+
+    # List of images in the image_path
+    imgs = glob.glob(image_path)
+    for i in imgs:
+        result = inference_detector(model, i)
+        res_border = []
+        res_bless = []
+        res_cell = []
+        tables = []
+        ## for border
+        for r in result[0][0]:
+            if r[4] > threshold:
+                res_border.append(r[:4].astype(int))
+        ## for cells
+        for r in result[0][1]:
+            if r[4] > threshold:
+                r[4] = r[4] * 100
+                res_cell.append(r.astype(int))
+        ## for borderless
+        for r in result[0][2]:
+            if r[4] > threshold:
+                res_bless.append(r[:4].astype(int))
+
+        ## if border tables detected
+        if len(res_border) != 0:
+            ## call border script for each table in image
+            for res in res_border:
+                try:
+                    tables.append(border(res, cv2.imread(i)))
+                except:
+                    pass
+        if len(res_bless) != 0:
+            if len(res_cell) != 0:
+                for no, res in enumerate(res_bless):
+                    tables.append(borderless(res, cv2.imread(i), res_cell))
+
+        print('got it')
+        print(tables)
+        print('-'*150)
+
+
+if __name__ == '__main__':
+    main()
